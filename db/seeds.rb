f0625f3296bf41eae1e7c0cb32f94d086e713973
Jordan Gangloff <jordangangloff@icloud.com>
2022-08-17 15:45:11 +0200

    no style but functionnal

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+6.times do
+  flat = Flat.new(
+    name: Faker::Hipster.sentence(word_count: 3, supplemental: false, random_words_to_add: 4),
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.paragraph(sentence_count: 3, supplemental: true),
+    price_per_night: Faker::Number.between(from: 50, to: 150),
+    number_of_guests: Faker::Number.between(from: 1, to: 6)
+  )
+  flat.save!
+end
